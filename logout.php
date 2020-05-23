f90c8490205a349d37abbb6de305152afdae0a2d
apiavis <agrant2589@gmail.com>
2020-05-23 10:42:56 -1000

    add Logout capability

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+    error_reporting(E_ALL ^ E_NOTICE);
+    require_once('Connect.php');
+    require 'master.php';
+
+    session_start();
+    unset($_SESSION['username']);  
+    header("Location: home.php");
+?>
